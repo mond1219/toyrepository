4a7eeffcba28f816a3404b14b3b90bbf9e499da0
doobyeol <doobyeol@gmail.com>
2021-01-05 21:28:00 +0900

    210105 21:27 KDB

---
+++ src/bitcampGit/Hello.java
@@ -0,0 +1,9 @@
+package bitcampGit;
+
+public class Hello {
+
+	public void say() {
+		System.out.println("Hello Java World!");
+	}
+
+}
---
+++ src/bitcampGit/HelloMain.java
@@ -0,0 +1,9 @@
+package bitcampGit;
+
+public class HelloMain {
+
+	public static void main(String[] args) {
+		new Hello().say();
+	}
+
+}
