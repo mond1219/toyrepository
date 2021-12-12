4fade4001a5a46891d8cd10c7a9f9026b6f8efb9
JiSuLee <JiSuLee@219.241.39.187>
2021-12-12 16:31:48 +0900

    B1008성공

--- Baekjoon/src/B1008.java
+++ Baekjoon/src/B1008.java
@@ -4,13 +4,11 @@ public class B1008 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		//정수형으로 입력받기
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		//계산결과는 실수형으로 
-		double dop = a/b;
-		System.out.println(dop);
-		//하지만 결과는 0.0이런식으로 나온다,,
+		//실수형으로 입력받기
+		Double a = sc.nextDouble();
+		Double b = sc.nextDouble();
+		//바로 계산결과 출력
+		System.out.println(a/b);
 	}
 
 }
