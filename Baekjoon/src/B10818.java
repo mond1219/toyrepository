78603f407e238f238c1830d87ee93df1e8b7e999
JiSuLee <JiSuLee@LEEJS-0901>
2021-12-14 10:48:12 +0900

    백준 10818 최대,최소 해결

--- Baekjoon/src/B10818.java
+++ Baekjoon/src/B10818.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class B10818 {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int arr[] = new int[sc.nextInt()];//입력받은 숫자만큼 배열 크기 선언
@@ -9,16 +8,28 @@ public class B10818 {
 		for(int i=0;i<arr.length;i++) {//배열 크기만큼 입력 받음
 			arr[i] = sc.nextInt();
 		}
-		for(int k=arr.length;k>0;k--) {//버블 정렬
-			for(int j=0;j<k-1;j++) {
-				if(arr[j]>arr[j+1]) {//뒤에 인수보다 앞에 인수가 크면
-					int temp = arr[j];
-					arr[j] = arr[j+1];//뒤에 값을 앞에 넣고
-					arr[j+1] = temp;//앞의 값을 뒤에 넣어준다.
+		int min;int max;
+		if(arr.length>1) {//배열의 크기가 2이상일때
+			//앞에 인덱스 0과 1의 크기를 비교하여 그둘을 일단 min, max로 설정
+			if(arr[0]>arr[1]) {
+				min=arr[1];max=arr[0];
+			}else {
+				min=arr[0];
+				max=arr[1];
+			}
+			//뒤에 값들과 비교하여 min보다 작으면 최소값 max보다 크면 최대값으로 다시 설정 
+			for(int j=2;j<arr.length;j++) {
+				if(arr[j]>max) {
+					max=arr[j];
+				}else if(arr[j]<min) {
+					min=arr[j];
 				}
 			}
+		}else{//배열의 크기가 1일때
+			min=arr[0];
+			max=arr[0];
 		}
-		System.out.println(arr[0]+" "+arr[arr.length-1]);
+		System.out.println(min+" "+max);
 	}
 
 }
