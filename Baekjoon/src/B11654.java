612524b39a55b669347a57e825f07175816409bc
lssgt07 <lssgt07@naver.com>
2021-12-16 09:56:08 +0900

    11654 백준 알고리즘

---
+++ Baekjoon/src/B11654.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class B11654 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		System.out.println(a.charAt(0)+0);
+		
+	}
+}
