6d289a3f45edda16914794fffe49a034e02b9e27
lssgt07 <lssgt07@naver.com>
2021-12-16 10:38:22 +0900

    1065 백준 문제풀이
    3자리 숫자뿐만 아니라 어떤 숫자를 입력받아도 한수를 출력할 수 있는 프로그램

--- Baekjoon/src/B1065.java
+++ Baekjoon/src/B1065.java
@@ -7,16 +7,36 @@ public class B1065 {
 			han=a;
 		}else {//입력받은 숫자가 세자리이상일때 
 			while(a>99) {
-				int inputNum = a;
-				int three = inputNum/100;//100자리 숫자 저장
-				inputNum=inputNum-three*100;
-				int two = inputNum/10;//2자리 숫자 저장
-				inputNum=inputNum-two*10;//1의 자리 숫자
-				if(three-two == two-inputNum) {//간격이 같다면
-					han=han+1;
+				//입력받은 숫자 문자열로 변환
+				String inputNum = a+"";
+				//입력받은 숫자 개수 체크
+				int numLength = inputNum.length();
+				int numArray[] = new int[numLength];
+				int gap[] = new int[numLength-1];//자리수 사이에 간격 저장 배열
+				
+				for(int i=0;i<numLength;i++) {
+					//숫자 앞자리 부터 1개씩 잘라서 정수로 변환하여 배열에 넣어주기 
+					numArray[i] = Integer.parseInt(inputNum.substring(i,i+1));
+					if(i>0) {
+						gap[i-1]=numArray[i]-numArray[i-1];//숫자 사이의 간격 저장해주기
+					}
+				}
+				//3자리 숫자 입력 받으면숫자 0,1,2 
+				//gap은 2개 0,1
+				numLength = numLength-1;
+				while(numLength>1) {
+					if(gap[numLength-1]!=gap[numLength-2]) {
+						break;
+					}else {
+						if(numLength==2) {//마지막 까지 전부 다 ture가 나왔다면
+							han=han+1;
+						}
+						numLength--;
+						
+					}
 				}
 				a--;
-			}
+			}	
 			han=han+99;
 			
 		}
