30fe7c3448be49107e120c8026b75e0c2f1dd61e
lssgt07 <lssgt07@naver.com>
2021-12-22 12:13:59 +0900

    2869 백준 알고리즘 문제풀이 시간초과문제 해결

--- Baekjoon/src/basicMath1/B2869.java
+++ Baekjoon/src/basicMath1/B2869.java
@@ -1,19 +1,23 @@
 package basicMath1;
-import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
 public class B2869 {
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		int dayTime = sc.nextInt();//낮에 올라갈 수 있는 길이
-		int nightTime = sc.nextInt();//밤에 내려가는 길이
-		int hight = sc.nextInt();//막대높이
-		int k=1;//날짜 카운트
-		while(true){
-			if(hight <= dayTime*k-nightTime*(k-1)) {
-				System.out.println(k);
-				break;
-			}
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int dayTime = Integer.parseInt(st.nextToken());//낮에 올라갈 수 있는 길이
+		int nightTime = Integer.parseInt(st.nextToken());//밤에 내려가는 길이
+		int hight = Integer.parseInt(st.nextToken());//막대높이
+		int k=(hight-nightTime)/(dayTime-nightTime);
+		if((hight-nightTime)%(dayTime-nightTime)!=0) {
+			//나머지가 존재하면 1일 더 추가
 			k++;
 		}
-		
+		System.out.println(k);
 	}
 }
