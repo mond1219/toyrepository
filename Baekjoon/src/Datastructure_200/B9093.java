3f1fd21cbf17802acdabc85ec300f2f176ff1d6e
lssgt07 <lssgt07@naver.com>
2022-01-04 11:37:59 +0900

    9093 백준알고리즘 문제풀이

--- Baekjoon/src/Datastructure_200/B9093.java
+++ Baekjoon/src/Datastructure_200/B9093.java
@@ -4,27 +4,25 @@ public class B9093 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int count = sc.nextInt();
-		
+		String arr[] = new String[count];
 		StringBuilder sb = new StringBuilder();
 		
-		
-		
-		 for (int i = 0; i < count; i++) {
-	            String str = sc.nextLine() + "\n"; 
-	            Stack st = new Stack<>();
-
-	            for (int j = 0; j < str.length(); j++) {
-	                if (str.charAt(j) == ' ' || str.charAt(j) == '\n') { 
-	                    while (!st.empty()) { 
-	                        sb.append(st.pop()); 
-	                    }
-	                    sb.append(str.charAt(j)); 
-	                } 
-	                else {
-	                    st.add(str.charAt(j)); 
-	                }
-	            }
-	        }
+		sc.nextLine();//Enter 입력 처리
+		for(int i=0;i<count;i++) {
+			String str = sc.nextLine()+"\n";
+			Stack st = new Stack();
+			for(int j=0;j<str.length();j++) {
+				if(str.charAt(j)==' ' || str.charAt(j)=='\n') {
+					//띄어쓰기 일때 
+					while(!st.isEmpty()) {
+						sb.append(st.pop());
+					}
+					sb.append(str.charAt(j));
+				}else {
+					st.add(str.charAt(j));
+				}
+			}
+		}
 		System.out.println(sb.toString());
 	}
 }
