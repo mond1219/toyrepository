78e1da8b03789599f0194cca2a3910d686424ae8
lssgt07 <lssgt07@naver.com>
2022-01-11 12:33:33 +0900

    17298 백준알고리즘 문제풀이

--- Baekjoon/src/Datastructure_201/B17298.java
+++ Baekjoon/src/Datastructure_201/B17298.java
@@ -2,7 +2,6 @@ package Datastructure_201;
 import java.util.*;
 import java.io.*;
 public class B17298 {
-	static int intArr[];
 	public static void main(String[] args) throws NumberFormatException, IOException  {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -11,28 +10,25 @@ public class B17298 {
 		int count = Integer.parseInt(br.readLine());
 		String result = br.readLine();
 		String arr[] = result.split(" ");
-		Queue<Integer> que = new LinkedList<Integer>();
-		intArr = new int[arr.length];
+		int intArr[] = new int[arr.length];
 		for(int i=0;i<arr.length;i++) {
-			que.add(Integer.parseInt(arr[i]));
 			intArr[i] = Integer.parseInt(arr[i]);
 		}
-		
-		for(int i=0;i<intArr.length;i++) {
-			int first = que.remove();
-			sb.append(check(i+1,first)+" ");
+		Stack<Integer> stack = new Stack<Integer>();
+		for(int i=0;i<count;i++) {
+			while(!stack.empty() && intArr[stack.peek()]<intArr[i]) {
+				intArr[stack.pop()] = intArr[i];
+			}
+			stack.push(i);
+		}
+		while(!stack.isEmpty()) {
+			intArr[stack.pop()] = -1;
+		}
+		for(int s:intArr) {
+			sb.append(s+" ");
 		}
 		bw.write(sb.toString());
 		bw.flush();
 		bw.close();	
 	}
-	static int check(int j, int one) {
-		for(;j<intArr.length;j++) {
-			if(intArr[j]>one) {
-				return intArr[j];
-			}
-		}
-		return -1;
-	}
-	
 }
