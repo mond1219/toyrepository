bdc02d25d5e2140821f6a88906c47fef6f15a32f
lssgt07 <lssgt07@naver.com>
2022-01-14 12:29:54 +0900

    1676 백준알고리즘 문제풀이

--- Baekjoon/src/Datastructure_300/B1676.java
+++ Baekjoon/src/Datastructure_300/B1676.java
@@ -4,28 +4,11 @@ public class B1676 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int number = sc.nextInt();
-		System.out.println(factory(number));
-		String[] result = (factory(number)+"").split("");
-		Stack<String> stack = new Stack<String>(); 
-		for(String s : result) {
-			stack.add(s);
-		}
 		int count =0;
-		while(!stack.isEmpty()) {
-			if(stack.pop().equals("0")) {
-				count++;
-			}else {
-				break;
-			}
+		while(number>=5) {
+			count = count+number/5;
+			number = number/5;
 		}
-		System.out.println(count);
-		
-	}
-	static int factory(double num) {
-		if(num ==0) {
-			return 1;
-		}
-		System.out.println((int)(num*factory(num-1)));
-		return (int)(num*factory(num-1));
+		System.out.println(count);	
 	}
 }
