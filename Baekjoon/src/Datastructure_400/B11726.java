e27886d462aa9e884d7c3471cfbf95fffe16f851
lssgt07 <lssgt07@naver.com>
2022-01-18 10:07:50 +0900

    11726 백준 알고리즘 문제풀이

--- Baekjoon/src/Datastructure_400/B11726.java
+++ Baekjoon/src/Datastructure_400/B11726.java
@@ -9,16 +9,18 @@ public class B11726 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
 		int num = Integer.parseInt(br.readLine());
-		System.out.println(check(num));
+		int[] count = new int[num+2];
+		count[0] = 1;
+		count[1] = 1;
+		count[2] = 2;
+		System.out.println(check(count,num));
 		
 	}
-	static int check(int num) {
-		if(num==1) {
-			return 1;
-		}else if(num ==2) {
-			return 2;
+	static int check(int[] count, int num) {
+		for(int i=2;i<=num;i++) {
+			count[i] = (count[i-1]+count[i-2])%10007;
 		}
-		return check(num-1)+check(num-2);
+		return count[num];
 	}
 
 }
