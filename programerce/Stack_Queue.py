ebd57e5d45fd3031f4d7c0ccf2489b909dc5639d
mond <76996008+mond1219@users.noreply.github.com>
2023-11-11 19:37:12 +0900

    Update Stack_Queue.py

--- programerce/Stack_Queue.py
+++ programerce/Stack_Queue.py
@@ -14,3 +14,27 @@ def hateSameNumber(arr):
             queue.append(quePOP)
         queue.append(now)
     return list(queue)
+
+# 기능 개발 
+# https://school.programmers.co.kr/learn/courses/30/lessons/42586
+
+import math
+def functionDev(progresses, speeds):
+    answer = []
+    cnt = 0
+    day = 0
+    for idx, num in enumerate(progresses):
+        if num + speeds[idx] * day >= 100:
+            cnt += 1
+        else:
+            if cnt > 0: # 현재까지 계산한 것 Answer에 담아주기
+                answer.append(cnt)
+
+            # 지금 num의 day 계산해주기
+            leftNum = 100 - num - day*speeds[idx] # 계산해야하는 progresse
+            day = day + math.ceil(leftNum/speeds[idx])
+            cnt = 1 # 1로 초기화 해주기
+
+    answer.append(cnt) # 마지막 배포하는 기능 개수 담아주기
+    return answer
+
