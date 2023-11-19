d16a6df784baf42c8d4d11721e56fd311c433267
mond <76996008+mond1219@users.noreply.github.com>
2023-11-19 15:03:40 +0900

    Update Hash.py

--- programerce/Hash.py
+++ programerce/Hash.py
@@ -40,3 +40,35 @@ def clothe(clothes):
     for key, value in clotheMap.items():
         answer = answer * (len(value)+1) # 옷의 개수 + 해당 옷의 종류를 안입고 있을 경우의 수
     return answer -1 # 모두 0인경우의 수 빼주기 
+
+
+# 베스트 앨범
+# https://school.programmers.co.kr/learn/courses/30/lessons/42579
+def best(genres, plays):
+    count = {}
+    idxMap  = {}
+    generMap = {}
+    for idx, i in enumerate(genres):
+        if i not in count:
+            count[i] = plays[idx]
+            generMap[i] = [idx]
+        else:
+            count[i] += plays[idx]
+            generMap[i].append(idx)
+        idxMap[idx] = plays[idx]
+    # 어떤 장르를 제일 먼저 해야하는가?  count의 value를 내림차순 정렬한 키값
+    cntList= sorted(count.items(), key=lambda x:x[1], reverse=True)
+    idxList = sorted(idxMap, key=lambda x:idxMap[x], reverse=True)
+
+    answer = []
+    for gener, count in cntList:
+        generSet = set(generMap[gener])
+        cnt = 1
+        for max in idxList:
+            if cnt >= 3:
+                break
+            if max in generSet:
+                answer.append(max)
+                cnt += 1
+
+    return answer
