3a519805e0188e9a4c0c9a8da76c3d966d3b642b
mond <76996008+mond1219@users.noreply.github.com>
2023-11-21 20:59:27 +0900

    Update recursion.py

--- programerce/recursion.py
+++ programerce/recursion.py
@@ -9,3 +9,13 @@ def coupon(chicken):
         cpCnt += int(chicken/10)
         chicken = int(chicken/10) + int(chicken%10)
     return int(cpCnt/10)
+
+# 콜라 문제
+# https://school.programmers.co.kr/learn/courses/30/lessons/132267
+# 이것도 재귀로,,안풀어졌다. 재귀로 푸는거 왜이렇게 어렵니,,ㅋ
+def coca(a, b, n):
+    answer = 0
+    while n >= a:
+        answer += int(n/a)*b
+        n = n - int(n/a)*a + int(n/a)*b
+    return answer
