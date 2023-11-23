2fa1e2f6a5601bbe1651be1c119ab836fe1bc264
mond <76996008+mond1219@users.noreply.github.com>
2023-11-23 20:59:25 +0900

    Update targetNumber.py

--- programerce/targetNumber.py
+++ programerce/targetNumber.py
@@ -13,3 +13,24 @@ inputList1 = [1, 1, 1, 1, 1]
 tarGet1 = 3
 result1 = 5
 targetNumber_BFS(inputList1, tarGet1)
+
+# [PCCP 기출문제] 1번 
+# https://school.programmers.co.kr/learn/courses/30/lessons/250137
+
+def monster(bandage, health, attacks):
+    nowHealth  = health
+    before = []
+
+    for attack in attacks:
+        atTime = attack[0]
+        demage = attack[1]
+        if len(before) != 0:
+            recoverTime = atTime - before[0] - 1
+            nowHealth += int(recoverTime/bandage[0]) * bandage[2] + recoverTime* bandage[1]
+            if nowHealth > health:
+                nowHealth = health
+        before = attack
+        nowHealth -= demage
+        if nowHealth <= 0:
+            return -1
+    return nowHealth
