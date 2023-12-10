84765e8f440ee6647f3a1b07619174a7853e1f50
mond <76996008+mond1219@users.noreply.github.com>
2023-12-10 20:26:05 +0900

    Update 일반수학1.py

--- Baekjoon/일반수학1.py
+++ Baekjoon/일반수학1.py
@@ -9,3 +9,19 @@ def numChange():
     for idx, n in enumerate(N):
         now_idx = alpaList.index(n)
         answer += n
+
+# 진법 변환2
+# https://www.acmicpc.net/problem/11005
+
+def numChange2():
+
+    N, B = input().split(' ')
+    N = int(N)
+    B = int(B)
+    alpaList= list("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+    answer =''
+    while N > 0:
+        answer += str(alpaList[N%B])
+        N = N//B
+        print(answer)
